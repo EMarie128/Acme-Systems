65c75b1631f1197d402aad0a78717e8bc88597ef
Emily Shirley <emarie12816@gmail.com>
2019-08-30 18:14:14 -0400

    Added another print statement

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -4,6 +4,8 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		System.out.println("This is a print statement");
+		System.out.println("Hello Dolly from Eclipse again");
+
 		
 	}
 
